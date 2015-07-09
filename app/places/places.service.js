3a4c2f0de4daea105f7cbb278a8d1198b5a0b4af
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-07-09 16:56:47 -0400

    trying to get a map to appear

--- app/places/places.service.js
+++ app/places/places.service.js
@@ -5,5 +5,14 @@
 angular.module('places')
   .factory('PlaceService', function($http) {
 
+    var map;
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 8,
+        center: {lat: -34.397, lng: 150.644}
+      });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
   });
 })();
