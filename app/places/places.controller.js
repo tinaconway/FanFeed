d92abaf1e9043c7ad36465ff0ce28465b53d7f9c
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-07-10 16:31:56 -0400

    little details

--- app/places/places.controller.js
+++ app/places/places.controller.js
@@ -6,7 +6,6 @@ angular.module('places')
   .controller('PlacesController', function($scope, $auth, $alert, Account) {
     $scope.map = { center: { latitude: 32.7833, longitude: -79.931051 }, zoom: 8 };
 
-  });
-
+  })
 
 })();
