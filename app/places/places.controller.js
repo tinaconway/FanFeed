038693643736ef87921ed7fa9836be673d38b2bd
Tina Conway <tinaconway@Tinas-MacBook-Air.local>
2015-07-12 17:32:37 -0400

    new home page

--- app/places/places.controller.js
+++ app/places/places.controller.js
@@ -6,82 +6,93 @@ var long;
 
 
 angular.module('places')
-  .controller('PlacesController', function($scope, $auth, $alert, Account, PlaceService, $routeParams, $sce) {
-    $scope.map = {
-        "center": {
-            "latitude": 32.7833,
-            "longitude": -79.931051
-        },
-        "zoom": 8
-    }; //TODO:  set location based on users current gps location
-    $scope.marker = {
-        id: 0,
-        coords: {
-            latitude: 52.47491894326404,
-            longitude: -1.8684210293371217
-        },
-        options: { draggable: true },
-        events: {
-            dragend: function (marker, eventName, args) {
+  .controller('PlacesController', function($scope, $auth, $alert, Account, PlaceService, $routeParams, $sce, uiGmapGoogleMapApi) {
 
-                $scope.marker.options = {
-                    draggable: true,
-                    labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-                    labelAnchor: "100 0",
-                    labelClass: "marker-labels"
-                };
-            }
-        }
-    };
-    var events = {
-        places_changed: function (searchBox) {
-            var place = searchBox.getPlaces();
-            lat = place[0].geometry.location.lat();
-            long = place[0].geometry.location.lng();
-            if (!place || place == 'undefined' || place.length == 0) {
-                return;
-            }
+    uiGmapGoogleMapApi.then(function(map) {
 
-            PlaceService.getBars(lat, long).then(function(data) {
-              $scope.places = data;
+      $scope.map = {
+          "center": {
+              "latitude": 32.7833,
+              "longitude": -79.931051
+          },
+          "zoom": 8
+      }; //TODO:  set location based on users current gps location
 
-            });
-            
-            if($routeParams.placeId) {
-              PlaceService.getSingleBar($routeParams.placeId).then(function(listing) {
-              console.log(listing);
-              $scope.place = listing;
+
+
+
+
+      });
+      $scope.marker = {
+          id: 0,
+          coords: {
+              latitude: 52.47491894326404,
+              longitude: -1.8684210293371217
+          },
+          options: { draggable: true },
+          events: {
+              dragend: function (marker, eventName, args) {
+
+                  $scope.marker.options = {
+                      draggable: true,
+                      labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                      labelAnchor: "100 0",
+                      labelClass: "marker-labels"
+                  };
+              }
+          }
+      };
+      var events = {
+          places_changed: function (searchBox) {
+              var place = searchBox.getPlaces();
+              lat = place[0].geometry.location.lat();
+              long = place[0].geometry.location.lng();
+              if (!place || place == 'undefined' || place.length == 0) {
+                  return;
+              }
+
+              PlaceService.getBars(lat, long).then(function(data) {
+                $scope.places = data;
+
               });
-            }
 
-            $scope.map = {
-                "center": {
-                    "latitude": place[0].geometry.location.lat(),
-                    "longitude": place[0].geometry.location.lng()
-                },
-                "zoom": 18
-            };
-            $scope.marker = {
-                id: 0,
-                coords: {
-                    latitude: place[0].geometry.location.lat(),
-                    longitude: place[0].geometry.location.lng()
-                }
-            };
+              if($routeParams.placeId) {
+                PlaceService.getSingleBar($routeParams.placeId).then(function(listing) {
+                console.log(listing);
+                $scope.place = listing;
+                });
+              }
 
+              $scope.map = {
+                  "center": {
+                      "latitude": place[0].geometry.location.lat(),
+                      "longitude": place[0].geometry.location.lng()
+                  },
+                  "zoom": 18
+              };
+              $scope.marker = {
+                  id: 0,
+                  coords: {
+                      latitude: place[0].geometry.location.lat(),
+                      longitude: place[0].geometry.location.lng()
+                  }
+              };
+
+          }
+      };
+
+      if($routeParams.placeId) {
+      PlaceService.getSingleBar($routeParams.placeId, lat, long).then(function(listing) {
+        $scope.place = listing;
+        $scope.reviews = listing.reviews;
+        $scope.trustSrc = function(src) {
+          return $sce.trustAsResourceUrl(src);
         }
-    };
-    if($routeParams.placeId) {
-    PlaceService.getSingleBar($routeParams.placeId, lat, long).then(function(listing) {
-      $scope.place = listing;
-      $scope.reviews = listing.reviews;
-      $scope.trustSrc = function(src) {
-        return $sce.trustAsResourceUrl(src);
+        console.log($scope.trustSrc(listing.website))
+      });
       }
-      console.log($scope.trustSrc(listing.website))
-    });
-    }
-    $scope.searchbox = { template: 'searchbox.tpl.html', events: events };
+
+      $scope.searchbox = { template: 'searchbox.tpl.html', events: events, parentdiv:'searchBoxParent' };
 
 
     $scope.createComment = function (newComment) {
--- app/places/places.module.js
+++ app/places/places.module.js
@@ -6,7 +6,7 @@ angular.module('places', [
 
 ])
 
-.config(function($routeProvider) {
+.config(function($routeProvider, uiGmapGoogleMapApiProvider) {
   $routeProvider
     .when('/places', {
       templateUrl: 'places/views/list.html',
@@ -16,7 +16,13 @@ angular.module('places', [
       templateUrl: 'places/views/detail.html',
       controller: 'PlacesController'
     })
-
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyDh3JutHi19Cdas8AyY36-R2Mn9mkMw-YA',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization,places'
+    });
   });
+
+
 
 })();
